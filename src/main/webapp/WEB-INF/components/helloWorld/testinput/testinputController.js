eccad5265c95dbfd4772f2f5a01dca628544c651
jason <jasonzhang2002@gmail.com>
2016-02-18 19:10:35 -0800

    first commit

---
+++ src/main/webapp/WEB-INF/components/helloWorld/testinput/testinputController.js
@@ -0,0 +1,13 @@
+({
+	sum: function(cmp, event){
+		var num1=0;
+		var num2=0;
+		if (cmp.get("v.num1")){
+			num1=cmp.get("v.num1");
+		}
+		if (cmp.get("v.num2")){
+			num2=cmp.get("v.num2");
+		}
+		cmp.set("v.sum", num1+num2);
+	}
+});
